893096422fa54b8c3cc69a84ad93f6b02d7a7070
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-01-26 22:21:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,12 @@ setuptools.setup(
         "Operating System :: POSIX",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: System :: Networking",
     ],
     entry_points={
