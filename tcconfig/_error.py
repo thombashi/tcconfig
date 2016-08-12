2d61b0a3236d4fb4d1138d84e482b93bd03320c7
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-08-12 16:23:00 +0900

    Initial commit

---
+++ tcconfig/_error.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+
+
+class TcCommandExecutionError(Exception):
+    """
+    Raised when a tc command failed.
+    """
