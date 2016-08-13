0b732279097463f4a2648a62be1ac77023da7e9f
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-08-13 10:20:12 +0900

    Change tc config loader to a class

--- tcconfig/tcset.py
+++ tcconfig/tcset.py
@@ -83,39 +83,6 @@ def parse_option():
     return parser.parser.parse_args()
 
 
-def _parse_tc_filter_network(text):
-    network_pattern = (
-        pp.SkipTo("network=", include=True) +
-        pp.Word(pp.alphanums + "." + "/"))
-
-    return network_pattern.parseString(text)[-1]
-
-
-def _parse_tc_filter_port(text):
-    port_pattern = (
-        pp.SkipTo("port=", include=True) +
-        pp.Word(pp.nums))
-
-    return port_pattern.parseString(text)[-1]
-
-
-def load_tcconfig(config_file):
-    from voluptuous import Schema, Required, Any, ALLOW_EXTRA
-
-    schema = Schema({
-        Required(six.text_type): {
-            Any("outgoing", "incoming"): {
-                six.text_type: {
-                    six.text_type: six.text_type,
-                },
-            }
-        },
-    }, extra=ALLOW_EXTRA)
-
-    return thutils.loader.JsonLoader.load(
-        config_file, schema)
-
-
 def verify_netem_module():
     runner = subprocrunner.SubprocessRunner("lsmod | grep sch_netem")
 
@@ -123,42 +90,81 @@ def verify_netem_module():
         raise ModuleNotFoundError("sch_netem module not found")
 
 
-def get_tcconfig_command_list(config_table, is_overwrite):
-    command_list = []
+class TcConfigLoader(object):
 
-    for device, device_table in six.iteritems(config_table):
-        if is_overwrite:
-            command_list.append("tcdel --device " + device)
+    def __init__(self):
+        self.__config_table = None
+        self.is_overwrite = False
 
-        for direction, direction_table in six.iteritems(device_table):
-            for tc_filter, filter_table in six.iteritems(direction_table):
-                if filter_table == {}:
-                    continue
+    def load_tcconfig(self, config_file_path):
+        import json
+        from voluptuous import Schema, Required, Any, ALLOW_EXTRA
 
-                option_list = [
-                    "--device=" + device,
-                    "--direction=" + direction,
-                ] + [
-                    "--{:s}={:s}".format(k, v)
-                    for k, v in six.iteritems(filter_table)
-                ]
+        schema = Schema({
+            Required(six.text_type): {
+                Any("outgoing", "incoming"): {
+                    six.text_type: {
+                        six.text_type: six.text_type,
+                    },
+                }
+            },
+        }, extra=ALLOW_EXTRA)
 
-                try:
-                    network = _parse_tc_filter_network(tc_filter)
-                    if network != "0.0.0.0/0":
-                        option_list.append("--network=" + network)
-                except pp.ParseException:
-                    pass
+        self.__config_table = json.loads(config_file_path)
+        schema(self.__config_table)
 
-                try:
-                    port = _parse_tc_filter_port(tc_filter)
-                    option_list.append("--port=" + port)
-                except pp.ParseException:
-                    pass
+    def get_tcconfig_command_list(self):
+        command_list = []
 
-                command_list.append(" ".join(["tcset"] + option_list))
+        for device, device_table in six.iteritems(self.__config_table):
+            if self.is_overwrite:
+                command_list.append("tcdel --device " + device)
 
-    return command_list
+            for direction, direction_table in six.iteritems(device_table):
+                for tc_filter, filter_table in six.iteritems(direction_table):
+                    if filter_table == {}:
+                        continue
+
+                    option_list = [
+                        "--device=" + device,
+                        "--direction=" + direction,
+                    ] + [
+                        "--{:s}={:s}".format(k, v)
+                        for k, v in six.iteritems(filter_table)
+                    ]
+
+                    try:
+                        network = self.__parse_tc_filter_network(tc_filter)
+                        if network != "0.0.0.0/0":
+                            option_list.append("--network=" + network)
+                    except pp.ParseException:
+                        pass
+
+                    try:
+                        port = self.__parse_tc_filter_port(tc_filter)
+                        option_list.append("--port=" + port)
+                    except pp.ParseException:
+                        pass
+
+                    command_list.append(" ".join(["tcset"] + option_list))
+
+        return command_list
+
+    @staticmethod
+    def __parse_tc_filter_network(text):
+        network_pattern = (
+            pp.SkipTo("network=", include=True) +
+            pp.Word(pp.alphanums + "." + "/"))
+
+        return network_pattern.parseString(text)[-1]
+
+    @staticmethod
+    def __parse_tc_filter_port(text):
+        port_pattern = (
+            pp.SkipTo("port=", include=True) +
+            pp.Word(pp.nums))
+
+        return port_pattern.parseString(text)[-1]
 
 
 def main():
@@ -180,9 +186,13 @@ def main():
     if dataproperty.is_not_empty_string(options.config_file):
         return_code = 0
 
-        for tcconfig_command in get_tcconfig_command_list(
-                load_tcconfig(options.config_file), options.overwrite):
-            return_code |= subprocrunner.SubprocessRunner.run(tcconfig_command)
+        loader = TcConfigLoader()
+        loader.is_overwrite = options.overwrite
+        loader.load_tcconfig(options.config_file)
+
+        for tcconfig_command in loader.get_tcconfig_command_list():
+            return_code |= subprocrunner.SubprocessRunner(
+                tcconfig_command).run()
 
         return return_code
 
