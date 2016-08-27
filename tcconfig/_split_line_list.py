28488a2afe4a8b7c798a6ccf90ce0f9464849bb1
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-08-27 23:46:18 +0900

    Optimize line strip processing

--- tcconfig/_split_line_list.py
+++ tcconfig/_split_line_list.py
@@ -9,15 +9,23 @@ from __future__ import unicode_literals
 import re
 
 
+def __null_line_strip(line):
+    return line
+
+
+def __line_strip(line):
+    return line.strip()
+
+
 def split_line_list(
         line_list, re_line_separator=re.compile("^$"),
         is_include_match_line=False, is_strip=True):
     block_list = []
     block = []
+    strip_func = __line_strip if is_strip else __null_line_strip
 
     for line in line_list:
-        if is_strip:
-            line = line.strip()
+        line = strip_func(line)
 
         if re_line_separator.search(line):
             if len(block) > 0:
