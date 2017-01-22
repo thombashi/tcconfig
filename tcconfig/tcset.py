9457f910908f5ca0570006ba6531495eff544553
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-01-23 00:27:27 +0900

    Prepara for 0.7.0 release

--- tcconfig/tcset.py
+++ tcconfig/tcset.py
@@ -48,10 +48,13 @@ def parse_option():
         help="""setting traffic controls from a configuration file.
         output file of the tcshow.""")
 
-    group = parser.parser.add_argument_group("Network Interface")
+    group = parser.parser.add_mutually_exclusive_group()
     group.add_argument(
         "--overwrite", action="store_true", default=False,
         help="overwrite existing settings")
+    group.add_argument(
+        "--add", dest="is_add_shaper", action="store_true", default=False,
+        help="add a traffic shaping rule in addition to existing rules.")
 
     group = parser.parser.add_argument_group("Traffic Control")
     group.add_argument(
@@ -102,22 +105,25 @@ def parse_option():
     group.add_argument(
         "--port", type=int,
         help="target port number to control traffic.")
-
-    group = parser.parser.add_argument_group("Experimental Prototype")
     group.add_argument(
-        "--add", dest="is_add_shaper", action="store_true", default=False,
-        help="add a traffic shaping rule in addition to existing rules.")
+        "--shaping-algo", dest="shaping_algorithm",
+        choices=["tbf", "htb"], default="htb",
+        help="shaping algorithm. defaults to %(default)s (recommended).")
+
+    group = parser.parser.add_argument_group("Routing")
     group.add_argument(
         "--iptables", dest="is_enable_iptables",
         action="store_true", default=False,
         help="use iptables to traffic shaping.")
     group.add_argument(
         "--src-network",
-        help="[require iptables]")
-    group.add_argument(
-        "--shaping-algo", dest="shaping_algorithm",
-        choices=["tbf", "htb"], default="htb",
-        help="shaping algorithm (default=%(default)s).")
+        help="""
+        set traffic shaping rule to a specific packets that routed from
+        --src-network to --network. This option required to execute with
+        the --iptables option. 
+        the shaping rule only affect to outgoing packets
+        (no effect to if you execute with "--direction incoming" option)
+        """)
 
     return parser.parser.parse_args()
 
@@ -165,17 +171,18 @@ class TcConfigLoader(object):
         command_list = []
 
         for device, device_table in six.iteritems(self.__config_table):
+            device_option = "--device={:s}".format(device)
+
             if self.is_overwrite:
-                command_list.append("tcdel --device " + device)
+                command_list.append("tcdel {:s}".format(device_option))
 
             for direction, direction_table in six.iteritems(device_table):
                 for tc_filter, filter_table in six.iteritems(direction_table):
-                    if filter_table == {}:
+                    if not filter_table:
                         continue
 
                     option_list = [
-                        "--device=" + device,
-                        "--direction=" + direction,
+                        device_option, "--direction={:s}".format(direction),
                     ] + [
                         "--{:s}={:s}".format(k, v)
                         for k, v in six.iteritems(filter_table)
