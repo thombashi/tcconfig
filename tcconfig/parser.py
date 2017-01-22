540c401a53a704248a15c9f5c58415404fd5c8a3
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-01-22 16:37:36 +0900

    Add TcClassParser class

--- tcconfig/parser.py
+++ tcconfig/parser.py
@@ -236,3 +236,55 @@ class TcQdiscParser(object):
                 self.__parsed_param[parse_param_name] = result
         except pp.ParseException:
             pass
+
+
+class TcClassParser(object):
+
+    class Pattern(object):
+        CLASS_ID = "[0-9a-z:]+"
+        RATE = "[0-9]+[KMGT]?"
+
+    class Key(object):
+        CLASS_ID = "classid"
+        RATE = "rate"
+
+    def __init__(self):
+        self.__clear()
+
+    def parse(self, text):
+        for line in text.splitlines():
+            self.__clear()
+
+            if dataproperty.is_empty_string(line):
+                continue
+
+            line = _to_unicode(line.lstrip())
+
+            self.__parse_classid(line)
+            self.__parse_rate(line)
+
+            yield self.__parsed_param
+
+    def __clear(self):
+        self.__parsed_param = {}
+
+    def __parse_classid(self, line):
+        self.__parsed_param[self.Key.CLASS_ID] = None
+        tag = "class htb "
+
+        match = re.search("{:s}{:s}".format(tag, self.Pattern.CLASS_ID), line)
+        if match is None:
+            return
+
+        self.__parsed_param[self.Key.CLASS_ID] = re.search(
+            self.Pattern.CLASS_ID, match.group().lstrip(tag)).group()
+
+    def __parse_rate(self, line):
+        self.__parsed_param[self.Key.RATE] = None
+
+        match = re.search("rate {:s}".format(self.Pattern.RATE), line)
+        if match is None:
+            return
+
+        self.__parsed_param[self.Key.RATE] = re.search(
+            self.Pattern.RATE, match.group()).group()
