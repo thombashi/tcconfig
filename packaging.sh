351cac490d8ed449b4395a8a28f886bc48a6f96f
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-01-23 00:40:10 +0900

    Initial commit

---
+++ packaging.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eu
+
+dist_dir="tcconfig_wheel"
+wheelhouse_dir_path=${dist_dir}/"wheelhouse"
+install_script_path=${dist_dir}/"install.sh"
+lancher_script="launcher.sh"
+
+
+# make wheelhouse ---
+python setup.py bdist_wheel --dist-dir ${dist_dir} --universal
+pip wheel -r requirements/requirements.txt --wheel-dir ${wheelhouse_dir_path}
+
+rm ${wheelhouse_dir_path}/Logbook-*.whl
+pip download Logbook --no-deps --dest ${wheelhouse_dir_path}
+
+
+# make an install script ---
+cat <<EOF > ${install_script_path}
+#!/bin/bash
+
+set -eu
+
+pip install --use-wheel --no-index --find-links=wheelhouse tcconfig-*.whl --upgrade
+EOF
+
+chmod +x ${install_script_path}
+
+
+# make an archive file ---
+tar cvf tcconfig_wheel.tar.gz tcconfig_wheel
