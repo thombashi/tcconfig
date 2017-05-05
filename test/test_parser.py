cab550eac9e90c5d2435f74d44cb3edcb8c3b83d
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-05-06 07:34:27 +0900

    Replace string literals to constants

--- test/test_parser.py
+++ test/test_parser.py
@@ -10,6 +10,7 @@ from __future__ import unicode_literals
 import pytest
 import six
 
+from tcconfig._const import Tc
 import tcconfig.parser
 
 
@@ -44,12 +45,12 @@ filter parent 1: protocol ip pref 2 u32 fh 800::800 order 2048 key ht 800 bkt 0 
   match 00000000/00000000 at 16"""),
             [
                 {
-                    'flowid': '1:1', 'network': '192.168.0.10/32',
-                    'protocol': 'ip', 'port': None,
+                    Tc.Param.FLOW_ID: '1:1', Tc.Param.NETWORK: '192.168.0.10/32',
+                    Tc.Param.PROTOCOL: 'ip', Tc.Param.PORT: None,
                 },
                 {
-                    'flowid': '1:2', 'network': '0.0.0.0/0',
-                    'protocol': None, 'port': None,
+                    Tc.Param.FLOW_ID: '1:2', Tc.Param.NETWORK: '0.0.0.0/0',
+                    Tc.Param.PROTOCOL: None, Tc.Param.PORT: None,
                 },
             ],
         ],
@@ -65,12 +66,12 @@ filter parent 1: protocol ip pref 2 u32 fh 800::800 order 2048 key ht 800 bkt 0 
   match 00000000/00000000 at 16"""),
             [
                 {
-                    'flowid': '1:1', 'network': '192.168.0.0/24',
-                    'protocol': 'ip', 'port': 80,
+                    Tc.Param.FLOW_ID: '1:1', Tc.Param.NETWORK: '192.168.0.0/24',
+                    Tc.Param.PROTOCOL: 'ip', Tc.Param.PORT: 80,
                 },
                 {
-                    'flowid': '1:2', 'network': '0.0.0.0/0',
-                    'protocol': None, 'port': None,
+                    Tc.Param.FLOW_ID: '1:2', Tc.Param.NETWORK: '0.0.0.0/0',
+                    Tc.Param.PROTOCOL: None, Tc.Param.PORT: None,
                 },
             ],
         ],
@@ -86,12 +87,12 @@ filter parent 1: protocol ip pref 2 u32 fh 800::800 order 2048 key ht 800 bkt 0 
   match 00000000/00000000 at 12"""),
             [
                 {
-                    'flowid': '1:3', 'network': '192.168.0.10/32',
-                    'protocol': 'ip', 'port': 8080,
+                    Tc.Param.FLOW_ID: '1:3', Tc.Param.NETWORK: '192.168.0.10/32',
+                    Tc.Param.PROTOCOL: 'ip', Tc.Param.PORT: 8080,
                 },
                 {
-                    'flowid': '1:2', 'network': '0.0.0.0/0',
-                    'protocol': None, 'port': None,
+                    Tc.Param.FLOW_ID: '1:2', Tc.Param.NETWORK: '0.0.0.0/0',
+                    Tc.Param.PROTOCOL: None, Tc.Param.PORT: None,
                 },
             ],
         ],
@@ -127,8 +128,8 @@ filter parent 1f87: protocol ipv6 pref 1 u32 fh 800::800 order 2048 key ht 800 b
   match 00000001/ffffffff at 36"""),
             [
                 {
-                    'flowid': '1f87:2', 'network': '::1/128',
-                    'protocol': 'ipv6', 'port': None,
+                    Tc.Param.FLOW_ID: '1f87:2', Tc.Param.NETWORK: '::1/128',
+                    Tc.Param.PROTOCOL: 'ipv6', Tc.Param.PORT: None,
                 },
             ]
         ],
@@ -142,8 +143,8 @@ filter parent 1f87: protocol ipv6 pref 1 u32 fh 800::800 order 2048 key ht 800 b
   match 00000001/ffffffff at 20"""),
             [
                 {
-                    'flowid': '1f87:2', 'protocol': 'ipv6',
-                    'network': '2001:db00::1/128', 'port': None
+                    Tc.Param.FLOW_ID: '1f87:2', Tc.Param.PROTOCOL: 'ipv6',
+                    Tc.Param.NETWORK: '2001:db00::1/128', Tc.Param.PORT: None
                 }
             ]
         ],
@@ -162,16 +163,16 @@ filter parent 1f87: protocol ipv6 pref 1 u32 fh 800::802 order 2050 key ht 800 b
   match 00001f90/0000ffff at 40"""),
             [
                 {
-                    'flowid': '1f87:2', 'protocol': 'ipv6',
-                    'network': '2001:db00::/24', 'port': None
+                    Tc.Param.FLOW_ID: '1f87:2', Tc.Param.PROTOCOL: 'ipv6',
+                    Tc.Param.NETWORK: '2001:db00::/24', Tc.Param.PORT: None
                 },
                 {
-                    'flowid': '1f87:3', 'protocol': None,
-                    'network': '2001:db00::1/128', 'port': None
+                    Tc.Param.FLOW_ID: '1f87:3', Tc.Param.PROTOCOL: None,
+                    Tc.Param.NETWORK: '2001:db00::1/128', Tc.Param.PORT: None
                 },
                 {
-                    'flowid': '1f87:4', 'protocol': None,
-                    'network': None, 'port': 8080
+                    Tc.Param.FLOW_ID: '1f87:4', Tc.Param.PROTOCOL: None,
+                    Tc.Param.NETWORK: None, Tc.Param.PORT: 8080
                 },
             ]
         ],
@@ -220,7 +221,7 @@ class Test_TcQdiscParser_parse(object):
             six.b("""qdisc htb 1f87: root refcnt 2 r2q 10 default 1 direct_packets_stat 1 direct_qlen 1000
 qdisc netem 2007: parent 1f87:2 limit 1000 delay 1.0ms loss 0.01%
 """),
-            [{'delay': '1.0', 'loss': '0.01', 'parent': '1f87:2'}],
+            [{'delay': '1.0', 'loss': '0.01', Tc.Param.PARENT: '1f87:2'}],
         ],
         [
             six.b("""
@@ -229,10 +230,10 @@ qdisc netem 2007: parent 1f87:2 limit 1000 delay 5.0ms
 qdisc netem 2008: parent 1f87:3 limit 1000 delay 50.0ms  1.0ms loss 5%
 """),
             [
-                {'delay': '5.0', 'parent': '1f87:2'},
+                {'delay': '5.0', Tc.Param.PARENT: '1f87:2'},
                 {
                     'delay': '50.0', 'loss': '5',
-                    'delay-distro': '1.0', 'parent': '1f87:3',
+                    'delay-distro': '1.0', Tc.Param.PARENT: '1f87:3',
                 },
             ],
         ],
