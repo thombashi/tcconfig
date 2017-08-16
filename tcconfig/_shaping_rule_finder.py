f61c0eb923bbdddbc6c2e828df61113edf754d43
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-08-16 17:42:29 +0900

    Replace is_exist_rule method internal processing by using the find_parent method

--- tcconfig/_shaping_rule_finder.py
+++ tcconfig/_shaping_rule_finder.py
@@ -59,36 +59,4 @@ class TcShapingRuleFinder(object):
         return parent
 
     def is_exist_rule(self):
-        key_param_list = (
-            Tc.Param.DST_NETWORK, Tc.Param.SRC_NETWORK,
-            Tc.Param.DST_PORT, Tc.Param.SRC_NETWORK,
-        )
-
-        search_filter = {
-            Tc.Param.DST_NETWORK: self.__tc.dst_network,
-            Tc.Param.SRC_NETWORK: self.__tc.src_network,
-            Tc.Param.DST_PORT: self.__tc.dst_port,
-            Tc.Param.SRC_NETWORK: self.__tc.src_port,
-        }
-
-        if self.__tc.direction == TrafficDirection.OUTGOING:
-            current_filter_list = self.__parser.get_outgoing_tc_filter()
-        elif self.__tc.direction == TrafficDirection.INCOMING:
-            current_filter_list = self.__parser.get_incoming_tc_filter()
-
-        self.__logger.debug(
-            "is_exist_rule: direction={}, search-filter={}, "
-            "current-filters={}".format(
-                self.__tc.direction, search_filter, current_filter_list))
-
-        for cuurent_filter in current_filter_list:
-            if all([
-                    cuurent_filter.get(
-                        key_param) == search_filter.get(key_param)
-                    for key_param in key_param_list
-            ]):
-                self.__logger.debug("existing shaping rule found: {}".format(
-                    cuurent_filter))
-                return True
-
-        return False
+        return self.find_parent() is not None
