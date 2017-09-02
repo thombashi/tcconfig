750f2bf82dba98e62d142a5f3615fac36a84601b
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-09-03 00:57:04 +0900

    Add a method to get valid time units

--- tcconfig/_converter.py
+++ tcconfig/_converter.py
@@ -143,6 +143,10 @@ class HumanReadableTime(object):
     def __repr__(self):
         return self.get_value()
 
+    @classmethod
+    def get_valid_unit_list(cls):
+        return cls.__VALID_UNIT_LIST
+
     def get_value(self):
         self.__preprocess()
 
