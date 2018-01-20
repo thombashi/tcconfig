25e2cb2aaad44fa900aa5d8b5200e7c4926a8375
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-01-21 02:10:17 +0900

    Bump version to 0.17.2

--- tcconfig/_const.py
+++ tcconfig/_const.py
@@ -8,7 +8,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 
-VERSION = "0.17.1"
+VERSION = "0.17.2"
 
 KILO_SIZE = 1000
 LIST_MANGLE_TABLE_COMMAND = "iptables -t mangle --line-numbers -L"
