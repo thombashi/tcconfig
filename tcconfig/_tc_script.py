646c33afe0404e36251f491cc60e74403d198d6d
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-08 12:26:45 +0900

    Modify __future__ import

--- tcconfig/_tc_script.py
+++ tcconfig/_tc_script.py
@@ -4,7 +4,7 @@
 .. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
 '''
 
-from __future__ import print_function, unicode_literals
+from __future__ import absolute_import, unicode_literals
 
 import datetime
 import io
