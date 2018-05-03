da410c718b284dab8ecdb0bdb8d655e401f9e730
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-05-04 00:41:19 +0900

    Update requirements

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setuptools.setup(
     extras_require={
         "build": build_requires,
         "docs": docs_requires,
-        "release": "releasecmd>=0.0.9",
+        "release": "releasecmd>=0.0.10",
         "test": tests_requires,
     },
 
