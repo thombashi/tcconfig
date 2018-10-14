19dbc31afd392eafe830de706042d6986d97bf06
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-14 22:50:13 +0900

    Move a function to a private method

--- tcconfig/tcdel.py
+++ tcconfig/tcdel.py
@@ -55,64 +55,12 @@ def parse_option():
     return parser.parser.parse_args()
 
 
-def create_tc_obj(tc_target, options):
-    from .parser.shaping_rule import TcShapingRuleParser
-    from simplesqlite.query import Where
-
-    if options.filter_id:
-        ip_version = 6 if options.is_ipv6 else 4
-        shaping_rule_parser = TcShapingRuleParser(
-            device=tc_target,
-            ip_version=ip_version,
-            tc_command_output=options.tc_command_output,
-            logger=logger,
-        )
-        shaping_rule_parser.parse()
-        result = shaping_rule_parser.con.select_as_dict(
-            table_name=TcSubCommand.FILTER.value,
-            column_list=[
-                Tc.Param.SRC_NETWORK,
-                Tc.Param.DST_NETWORK,
-                Tc.Param.SRC_PORT,
-                Tc.Param.DST_PORT,
-            ],
-            where=Where(Tc.Param.FILTER_ID, options.filter_id),
-        )
-        if not result:
-            logger.error(
-                "shaping rule not found associated with the id ({}).".format(options.filter_id)
-            )
-            sys.exit(1)
-
-        filter_param = result[0]
-        dst_network = filter_param.get(Tc.Param.DST_NETWORK)
-        src_network = filter_param.get(Tc.Param.SRC_NETWORK)
-        dst_port = filter_param.get(Tc.Param.DST_PORT)
-        src_port = filter_param.get(Tc.Param.SRC_PORT)
-    else:
-        dst_network = options.dst_network
-        src_network = options.src_network
-        dst_port = options.dst_port
-        src_port = options.src_port
-
-    return TrafficControl(
-        tc_target,
-        direction=options.direction,
-        dst_network=dst_network,
-        src_network=src_network,
-        dst_port=dst_port,
-        src_port=src_port,
-        is_ipv6=options.is_ipv6,
-        tc_command_output=options.tc_command_output,
-    )
-
-
 class TcDelMain(Main):
     def run(self, is_delete_all):
         return_code = 0
 
         for tc_target in self._fetch_tc_target_list():
-            tc = create_tc_obj(tc_target, self._options)
+            tc = self.__create_tc_obj(tc_target)
             if self._options.log_level == logbook.INFO:
                 spr.set_log_level(logbook.ERROR)
             normalize_tc_value(tc)
@@ -129,6 +77,59 @@ class TcDelMain(Main):
             self._dump_history(tc, Tc.Command.TCDEL)
 
         return return_code
+
+    def __create_tc_obj(self, tc_target):
+        from .parser.shaping_rule import TcShapingRuleParser
+        from simplesqlite.query import Where
+
+        options = self._options
+
+        if options.filter_id:
+            ip_version = 6 if options.is_ipv6 else 4
+            shaping_rule_parser = TcShapingRuleParser(
+                device=tc_target,
+                ip_version=ip_version,
+                tc_command_output=options.tc_command_output,
+                logger=logger,
+            )
+            shaping_rule_parser.parse()
+            result = shaping_rule_parser.con.select_as_dict(
+                table_name=TcSubCommand.FILTER.value,
+                column_list=[
+                    Tc.Param.SRC_NETWORK,
+                    Tc.Param.DST_NETWORK,
+                    Tc.Param.SRC_PORT,
+                    Tc.Param.DST_PORT,
+                ],
+                where=Where(Tc.Param.FILTER_ID, options.filter_id),
+            )
+            if not result:
+                logger.error(
+                    "shaping rule not found associated with the id ({}).".format(options.filter_id)
+                )
+                sys.exit(1)
+
+            filter_param = result[0]
+            dst_network = filter_param.get(Tc.Param.DST_NETWORK)
+            src_network = filter_param.get(Tc.Param.SRC_NETWORK)
+            dst_port = filter_param.get(Tc.Param.DST_PORT)
+            src_port = filter_param.get(Tc.Param.SRC_PORT)
+        else:
+            dst_network = options.dst_network
+            src_network = options.src_network
+            dst_port = options.dst_port
+            src_port = options.src_port
+
+        return TrafficControl(
+            tc_target,
+            direction=options.direction,
+            dst_network=dst_network,
+            src_network=src_network,
+            dst_port=dst_port,
+            src_port=src_port,
+            is_ipv6=options.is_ipv6,
+            tc_command_output=options.tc_command_output,
+        )
 
 
 def main():
