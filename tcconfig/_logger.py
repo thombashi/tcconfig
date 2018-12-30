d96d273586c1c9f5652b4b1271333ae47289f581
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-12-30 19:25:29 +0900

    Modify logger configuration functions

--- tcconfig/_logger.py
+++ tcconfig/_logger.py
@@ -16,10 +16,17 @@ logger.disable()
 
 
 def set_logger(is_enable):
+    if is_enable != logger.disabled:
+        # logger setting have not changed
+        return
+
     if is_enable:
         logger.enable()
     else:
         logger.disable()
+
+    simplesqlite.set_logger(is_enable)
+    subprocrunner.set_logger(is_enable)
 
 
 def set_log_level(log_level):
@@ -36,6 +43,9 @@ def set_log_level(log_level):
 
     # validate log level
     logbook.get_level_name(log_level)
+
+    if log_level == logger.level:
+        return
 
     if log_level == logbook.NOTSET:
         set_logger(is_enable=False)
