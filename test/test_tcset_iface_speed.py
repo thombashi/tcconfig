845fbc5a0a9af8012813e4945bf6e71c0dab7fb7
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-04-14 02:52:03 +0900

    Update test cases

--- test/test_tcset_iface_speed.py
+++ test/test_tcset_iface_speed.py
@@ -25,27 +25,43 @@ class Test_tcset_iface_speed(object):
         if device_option is None:
             pytest.skip("device option is null")
 
-        for tc_target in [device_option, "--device {}".format(device_option)]:
-            execute_tcdel(tc_target)
+        for tc_target in [device_option]:
             monkeypatch.setattr("tcconfig._network._read_iface_speed", lambda x: speed)
 
-            command_list = [Tc.Command.TCSET, tc_target, "--rate {:s}".format("1kbps")]
-            assert SubprocessRunner(" ".join(command_list)).run() == 0
+            runner = SubprocessRunner(
+                [Tc.Command.TCSET, tc_target, "--rate", "1kbps", "--overwrite"]
+            )
+            assert runner.run() == 0, (runner.command_str, runner.returncode, runner.stderr)
 
             # finalize ---
             execute_tcdel(tc_target)
 
-    @pytest.mark.parametrize(["rate"], [["0kbps"], ["99Gbps"]])
+    @pytest.mark.parametrize(["rate"], [["990Gbps"]])
+    def test_normal_exceed_max_rate(self, monkeypatch, device_option, rate):
+        if device_option is None:
+            pytest.skip("device option is null")
+
+        for tc_target in [device_option]:
+            monkeypatch.setattr("tcconfig._network._read_iface_speed", lambda x: "1")
+
+            runner = SubprocessRunner([Tc.Command.TCSET, tc_target, "--rate", rate, "--overwrite"])
+            assert runner.run() == 0, (runner.command_str, runner.returncode, runner.stderr)
+
+            # finalize ---
+            execute_tcdel(tc_target)
+
+    @pytest.mark.parametrize(["rate"], [["0kbps"]])
     def test_abnormal(self, monkeypatch, device_option, rate):
         if device_option is None:
             pytest.skip("device option is null")
 
-        for tc_target in [device_option, "--device {}".format(device_option)]:
-            execute_tcdel(tc_target)
+        for tc_target in [device_option]:
             monkeypatch.setattr("tcconfig._network._read_iface_speed", lambda x: "1")
 
-            command_list = [Tc.Command.TCSET, tc_target, "--rate {:s}".format(rate)]
-            assert SubprocessRunner(" ".join(command_list)).run() != 0
+            runner = SubprocessRunner(
+                " ".join([Tc.Command.TCSET, tc_target, "--rate", rate, "--overwrite"])
+            )
+            assert runner.run() != 0, (runner.command_str, runner.returncode, runner.stderr)
 
             # finalize ---
             execute_tcdel(tc_target)
