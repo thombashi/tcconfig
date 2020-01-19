a90f25d1b8cb694aea8b219050354ec7aba91d91
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-19 16:24:07 +0900

    Modify tcshow output for docker container

--- tcconfig/tcshow.py
+++ tcconfig/tcshow.py
@@ -157,9 +157,7 @@ def extract_tc_params(options):
                         export_settings(options.export_path, out_rules, in_rules)
 
                     tc_params.update(rule_parser.get_tc_parameter())
-                    key = "{veth} (container_id={id}, image={image})".format(
-                        veth=veth, id=container_info.id[:12], image=container_info.image
-                    )
+                    key = "{id} (device={veth})".format(id=container_info.id[:12], veth=veth,)
                     tc_params[key] = tc_params.pop(veth)
             else:
                 verify_network_interface(device, options.tc_command_output)
