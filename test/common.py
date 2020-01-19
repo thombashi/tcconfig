38402da8b6e926dc39b2afd7e6d6d9edcb8fe471
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-19 14:57:50 +0900

    Add NullLogger class for test

--- test/common.py
+++ test/common.py
@@ -67,3 +67,43 @@ def runner_helper(command):
     assert proc_runner.returncode == 0
 
     return proc_runner
+
+
+class NullLogger(object):
+    level_name = None
+
+    def catch_exceptions(self, *args, **kwargs):  # pragma: no cover
+        pass
+
+    def critical(self, *args, **kwargs):  # pragma: no cover
+        pass
+
+    def debug(self, *args, **kwargs):  # pragma: no cover
+        pass
+
+    def disable(self):  # pragma: no cover
+        pass
+
+    def enable(self):  # pragma: no cover
+        pass
+
+    def error(self, *args, **kwargs):  # pragma: no cover
+        pass
+
+    def exception(self, *args, **kwargs):  # pragma: no cover
+        pass
+
+    def info(self, *args, **kwargs):  # pragma: no cover
+        pass
+
+    def log(self, level, *args, **kwargs):  # pragma: no cover
+        pass
+
+    def notice(self, *args, **kwargs):  # pragma: no cover
+        pass
+
+    def warn(self, *args, **kwargs):  # pragma: no cover
+        pass
+
+    def warning(self, *args, **kwargs):  # pragma: no cover
+        pass
