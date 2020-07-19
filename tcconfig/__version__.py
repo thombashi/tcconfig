2173ffc4fa4e23fa0a2b89c1185e9e44350d5aad
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-07-20 01:14:41 +0900

    Bump version

--- tcconfig/__version__.py
+++ tcconfig/__version__.py
@@ -1,6 +1,6 @@
 __author__ = "Tsuyoshi Hombashi"
 __copyright__ = "Copyright 2016, {}".format(__author__)
 __license__ = "MIT License"
-__version__ = "0.25.2"
+__version__ = "0.25.3"
 __maintainer__ = __author__
 __email__ = "tsuyoshi.hombashi@gmail.com"
