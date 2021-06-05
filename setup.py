3e59d8d5bfd3028fdba35a37d1dcae0060595997
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-06-05 22:25:09 +0900

    Add support for Python 3.9

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: System :: Operating System Kernels :: Linux",
         "Topic :: System :: Networking",
         "Topic :: System :: Systems Administration",
