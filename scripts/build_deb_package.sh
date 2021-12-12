8f9ae876f8725ec976c51abdcafbea66c5fcc5b2
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-12-12 21:08:19 +0900

    Bump minimum pip version at a build script

--- scripts/build_deb_package.sh
+++ scripts/build_deb_package.sh
@@ -13,7 +13,7 @@ cd "$(git rev-parse --show-toplevel)"
 rm -rf $DIST_DIR_NAME
 mkdir -p "${DIST_DIR_NAME}/DEBIAN"
 
-pip install --upgrade "pip>=19.0.2"
+pip install --upgrade "pip>=21.1"
 pip install --upgrade .[buildexe,color]
 
 PKG_VERSION=$(python -c "import ${PKG_NAME}; print(${PKG_NAME}.__version__)")
