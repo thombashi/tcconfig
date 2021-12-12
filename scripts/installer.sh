3366c7c2140a7cf4d61f5d61e71aa1d9dc28e628
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-12-13 00:31:02 +0900

    Update installer script

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -7,9 +7,11 @@ if [ $UID -ne 0 ]; then
     exit 13
 fi
 
-ARCHIVE_URL=$(curl -sSL https://raw.githubusercontent.com/thombashi/tcconfig/master/info/release_latest.json | jq -r '.assets[].browser_download_url' | \grep deb)
-TEMP_DEB="$(mktemp)"
+VERSION_CODENAME=$(\grep -Po "(?<=VERSION_CODENAME=)[a-z]+" /etc/os-release)
+ARCHIVE_URL=$(curl -sSL https://raw.githubusercontent.com/thombashi/tcconfig/master/info/release_latest.json | jq -r '.assets[].browser_download_url' | \grep -E "tcconfig_.+_${VERSION_CODENAME}_amd64.deb")
+TEMP_DIR="$(mktemp -d)"
+TEMP_DEB="${TEMP_DIR}/$(basename ${ARCHIVE_URL})"
 
-trap "\rm -f $TEMP_DEB" 0 1 2 3 15
+trap "\rm -rf $TEMP_DIR" 0 1 2 3 15
 curl -L "$ARCHIVE_URL" -o "$TEMP_DEB"
 dpkg -i "$TEMP_DEB"
