aec00900a1b2741c91838c3cc3aad95f19812e4f
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2022-09-26 01:35:37 +0900

    Refactoring

--- scripts/build_deb_package.sh
+++ scripts/build_deb_package.sh
@@ -14,8 +14,8 @@ cd "$(git rev-parse --show-toplevel)"
 rm -rf $DIST_DIR_NAME
 mkdir -p "${DIST_DIR_NAME}/DEBIAN"
 
-pip install --upgrade "pip>=21.1"
-pip install --upgrade .[buildexe,color]
+$PYTHON -m pip install --upgrade "pip>=21.1"
+$PYTHON -m pip install --upgrade .[buildexe,color]
 
 PKG_VERSION=$($PYTHON -c "import ${PKG_NAME}; print(${PKG_NAME}.__version__)")
 
