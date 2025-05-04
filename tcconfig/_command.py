4f8fe41a66ebc1d84b1d2dd768a7d0e53ea073d7
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2025-05-05 00:42:26 +0900

    Extract Main subclasses
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

---
+++ tcconfig/_command.py
@@ -0,0 +1,84 @@
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+import errno
+import sys
+
+import subprocrunner as spr
+
+from ._common import normalize_tc_value
+from ._const import Tc
+from ._error import NetworkInterfaceNotFoundError
+from ._logger import LogLevel, logger
+from ._main import Main
+from .parser._model import Filter
+from .traffic_control import TrafficControl
+
+
+class TcDelMain(Main):
+    def run(self, is_delete_all):
+        return_code_list = []
+
+        for tc_target in self._fetch_tc_targets():
+            tc = self.__create_tc_obj(tc_target)
+            if self._options.log_level == LogLevel.INFO:
+                spr.set_log_level("ERROR")
+            normalize_tc_value(tc)
+
+            try:
+                if is_delete_all:
+                    return_code_list.append(0 if tc.delete_all_rules() is True else 1)
+                else:
+                    return_code_list.append(tc.delete_tc())
+            except NetworkInterfaceNotFoundError as e:
+                logger.error(e)
+                return errno.EINVAL
+
+            self._dump_history(tc, Tc.Command.TCDEL)
+
+        return self._get_return_code(return_code_list)
+
+    def __create_tc_obj(self, tc_target):
+        from simplesqlite.query import Where
+
+        from .parser.shaping_rule import TcShapingRuleParser
+
+        options = self._options
+
+        if options.filter_id:
+            ip_version = 6 if options.is_ipv6 else 4
+            shaping_rule_parser = TcShapingRuleParser(
+                device=tc_target,
+                ip_version=ip_version,
+                tc_command_output=options.tc_command_output,
+                logger=logger,
+            )
+            shaping_rule_parser.parse()
+            for record in Filter.select(where=Where(Tc.Param.FILTER_ID, options.filter_id)):
+                dst_network = record.dst_network
+                src_network = record.src_network
+                dst_port = record.dst_port
+                src_port = record.src_port
+                break
+            else:
+                logger.error(
+                    f"shaping rule not found associated with the id ({options.filter_id})."
+                )
+                sys.exit(1)
+        else:
+            dst_network = self._extract_dst_network()
+            src_network = self._extract_src_network()
+            dst_port = options.dst_port
+            src_port = options.src_port
+
+        return TrafficControl(
+            tc_target,
+            direction=options.direction,
+            dst_network=dst_network,
+            src_network=src_network,
+            dst_port=dst_port,
+            src_port=src_port,
+            is_ipv6=options.is_ipv6,
+            tc_command_output=options.tc_command_output,
+        )
--- tcconfig/tcdel.py
+++ tcconfig/tcdel.py
@@ -12,14 +12,11 @@ import subprocrunner as spr
 from .__version__ import __version__
 from ._argparse_wrapper import ArgparseWrapper
 from ._capabilities import check_execution_authority
-from ._common import initialize_cli, is_execute_tc_command, normalize_tc_value
-from ._const import Tc
+from ._command import TcDelMain
+from ._common import initialize_cli, is_execute_tc_command
 from ._error import NetworkInterfaceNotFoundError
-from ._logger import LogLevel, logger, set_logger
-from ._main import Main
+from ._logger import logger, set_logger
 from ._network import verify_network_interface
-from .parser._model import Filter
-from .traffic_control import TrafficControl
 
 
 def parse_option():
@@ -51,74 +48,6 @@ def parse_option():
     parser.add_docker_group()
 
     return parser.parser.parse_args()
-
-
-class TcDelMain(Main):
-    def run(self, is_delete_all):
-        return_code_list = []
-
-        for tc_target in self._fetch_tc_targets():
-            tc = self.__create_tc_obj(tc_target)
-            if self._options.log_level == LogLevel.INFO:
-                spr.set_log_level("ERROR")
-            normalize_tc_value(tc)
-
-            try:
-                if is_delete_all:
-                    return_code_list.append(0 if tc.delete_all_rules() is True else 1)
-                else:
-                    return_code_list.append(tc.delete_tc())
-            except NetworkInterfaceNotFoundError as e:
-                logger.error(e)
-                return errno.EINVAL
-
-            self._dump_history(tc, Tc.Command.TCDEL)
-
-        return self._get_return_code(return_code_list)
-
-    def __create_tc_obj(self, tc_target):
-        from simplesqlite.query import Where
-
-        from .parser.shaping_rule import TcShapingRuleParser
-
-        options = self._options
-
-        if options.filter_id:
-            ip_version = 6 if options.is_ipv6 else 4
-            shaping_rule_parser = TcShapingRuleParser(
-                device=tc_target,
-                ip_version=ip_version,
-                tc_command_output=options.tc_command_output,
-                logger=logger,
-            )
-            shaping_rule_parser.parse()
-            for record in Filter.select(where=Where(Tc.Param.FILTER_ID, options.filter_id)):
-                dst_network = record.dst_network
-                src_network = record.src_network
-                dst_port = record.dst_port
-                src_port = record.src_port
-                break
-            else:
-                logger.error(
-                    f"shaping rule not found associated with the id ({options.filter_id})."
-                )
-                sys.exit(1)
-        else:
-            dst_network = self._extract_dst_network()
-            src_network = self._extract_src_network()
-            dst_port = options.dst_port
-            src_port = options.src_port
-
-        return TrafficControl(
-            tc_target,
-            direction=options.direction,
-            dst_network=dst_network,
-            src_network=src_network,
-            dst_port=dst_port,
-            src_port=src_port,
-            is_ipv6=options.is_ipv6,
-            tc_command_output=options.tc_command_output,
-        )
 
 
 def main():
